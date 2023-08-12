75d3f78ea82008d6b1b95744ee63c8802b43c94d
hariwr <hariwr25@gmail.com>
2023-08-12 12:58:22 +0700

    Tugas UTS

---
+++ src/main/java/com/uts/tugasuts/TugasutsApplication.java
@@ -0,0 +1,13 @@
+package com.uts.tugasuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uts/tugasuts/TugasutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uts.tugasuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
